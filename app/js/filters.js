27a52ab5b7f63f75211c9896b7e095b80c3a915e
Trevor Kaufman <tskaufma@hotmail.com>
2013-11-08 11:58:05 -0500

    Minor UI improvements, better data on listing page

--- app/js/filters.js
+++ app/js/filters.js
@@ -16,6 +16,28 @@ angular.module('myApp.filters', []).
         start = +start; //parse to int
         return input.slice(start);
     }
+  }).
+
+  // Create filter to parse dates
+  filter('parseDate', function() {
+      var result = function(dateStr, formatStr) {
+          var date = null;
+          if (formatStr === null) {
+              date = moment(dateStr);
+          } else {
+              date = moment(dateStr, formatStr);
+          }
+
+          return date.toDate();
+      }
+      return result;
+  }).
+
+  // list of objects with a 'name' to a comma seperated list
+  filter('listNames', function() {
+      return function(list) {
+        return list.map(function(item) {return item.name}).join(", ");
+      };
   })
 ;
 
